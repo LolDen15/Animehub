774ee43e621e441c65c5cbea45b168900073fc60
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-11-13 20:34:03 +0300

    .

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -132,27 +132,24 @@ function initSliders() {
 			*/
 
 			// Брейкпоинты
-			/*
 			breakpoints: {
 				320: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
+					slidesPerView: 'auto',
+					spaceBetween: 10,
 				},
-				768: {
+				900: {
 					slidesPerView: 2,
 					spaceBetween: 20,
 				},
-				992: {
+				1300: {
 					slidesPerView: 3,
 					spaceBetween: 20,
 				},
-				1268: {
+				1700: {
 					slidesPerView: 4,
 					spaceBetween: 30,
 				},
 			},
-			*/
 			// События
 			on: {
 
